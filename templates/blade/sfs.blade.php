f89244fbd5422b57fc761453d1f099b7b9669a75
Malcolm Elsworth <malcolm@electricputty.co.uk>
2017-10-03 10:51:35 +0100

    Fixed typos in Laravel Blade

--- templates/blade/sfs.blade.php
+++ templates/blade/sfs.blade.php
@@ -169,7 +169,7 @@
 							<tr>
 								<td class="sfs__label">Total income</td>
 								<td class="sfs__table-highlight sfs__output sfs__number_box" id="total-monthly-income">
-									&pound;{{ number_format($sfs['total-monthly-income'], 2) }}
+									&pound;{{ number_format($sfs['overview']['total-monthly-income'], 2) }}
 								</td>
 							</tr>
 							<tr>
@@ -242,7 +242,7 @@
 							<tr class="sfs__total">
 								<td class="sfs__label">Total income</td>
 								<td class="sfs__table-highlight sfs__output sfs__number_box" id="total-monthly-income">
-									&pound;{{ number_format($sfs['monthly-income']['total-monthly-incomes'], 2) }}
+									&pound;{{ number_format($sfs['monthly-income']['total-monthly-income'], 2) }}
 								</td>
 							</tr>
 						</table>
